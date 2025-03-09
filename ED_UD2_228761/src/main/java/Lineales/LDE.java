68afabb739a35a4859b875a8238d9ef911d49968
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2025-03-09 12:55:18 -0700

    Estructura basica del proyecto
    Se agrego la estructura basica del proyecto para posteriormente implementar los elementos necesarios para el correcto funcionamiento de una lista enlazada simple

---
+++ ED_UD2_228761/src/main/java/Lineales/LDE.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lineales;
+
+import com.mycompany.ed_ud2_228761.Nodos.NodoD;
+
+/**
+ *
+ * @author gaspa
+ */
+public class LDE {
+    private NodoD inicial;
+    private int tam = 0;
+
+    public LDE() {
+    }
+    
+    public boolean addLast(int valor){
+        NodoD nN = new NodoD(valor);
+        if(tam == 0){
+            inicial = nN;
+            tam++;
+            return true;
+        }else{
+            NodoD actual = inicial;
+            while(actual.getSig() != null){
+                actual = actual.getSig();
+            }
+            actual.setSig(nN);
+            nN.setAnt(actual);
+            tam++;
+            return true;
+        }
+    }
+    public void print(){
+        NodoD actual = inicial;
+        
+        while(actual!= null){
+            System.out.println(actual.toString());
+            actual = actual.getSig();
+        }
+    }
+}
---
+++ ED_UD2_228761/src/main/java/com/mycompany/ed_ud2_228761/ED_UD2_228761.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ed_ud2_228761;
+
+/**
+ *
+ * @author gaspa
+ */
+public class ED_UD2_228761 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
