9a48ccd555be375136d70573b47dacaf1ff9dc98
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2025-03-28 15:35:46 -0700

    Metoddos de LDE
    se agregaron los metodos faltantes de una LDE

--- ED_UD2_228761/src/main/java/Lineales/LDE.java
+++ ED_UD2_228761/src/main/java/Lineales/LDE.java
@@ -8,13 +8,31 @@ import Nodos.NodoD;
 
 /**
  *
- * @author gaspa
+ * @author Ilian Fernando Gastelum Romo
  */
 public class LDE {
     private NodoD inicial;
     private int tam = 0;
 
     public LDE() {
+    }
+    public boolean isEmpty(){
+        return tam <= 0;
+    }
+    public int size(){
+        return tam;
+    }
+    public boolean addFirst(int valor) {
+        NodoD nN = new NodoD(valor);
+        if (tam == 0) {
+            inicial = nN;
+        } else {
+            nN.setSig(inicial);
+            inicial.setAnt(nN);
+            inicial = nN;
+        }
+        tam++;
+        return true;
     }
     
     public boolean addLast(int valor){
@@ -33,6 +51,56 @@ public class LDE {
             tam++;
             return true;
         }
+    }
+    public void removeFirst(){
+        if (tam == 1) {
+            inicial=null;
+            tam--;
+        }else if(tam>1){
+            inicial = inicial.getSig();
+            inicial.getAnt().setSig(null);
+            inicial.setAnt(null);
+            tam--;
+        }
+    }
+    public void removeLast() {
+        if (tam == 1) {
+            inicial = null;
+        } else if (tam > 1) {
+            NodoD actual = inicial;
+            while (actual.getSig() != null) {
+                actual = actual.getSig();
+            }
+            actual.getAnt().setSig(null);
+        }
+        tam = Math.max(0, tam - 1);
+    }
+    public boolean contains(int val){
+        NodoD actual = inicial;
+        boolean b=false;
+        while(actual !=null){
+            if (actual.getValor()==val) {
+                b = true;
+                break;
+            }else{
+                actual=actual.getSig();
+            }  
+        }
+        return b;
+    }
+    public int getElement(int index) {
+        if (index < 0 || index >= tam) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango.");
+        }
+        NodoD actual = inicial;
+        for (int i = 0; i < index; i++) {
+            actual = actual.getSig();
+        }
+        return actual.getValor();
+    }
+    public void clear(){
+        inicial = null;
+        tam = 0;
     }
     public void print(){
         NodoD actual = inicial;
